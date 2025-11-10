d66e018e019c64530abe0c3c9fcc57c18bd996af
KarenGarciaDev <ale1223draa@gmail.com>
2025-11-09 23:00:51 -0500

    Update app.js
    "fix vercel deployment"

--- app.js
+++ app.js
@@ -4,13 +4,8 @@
 const express = require('express');
 const app = express();
 
-// Middleware to parse JSON (optional)
-app.use(express.json());
-
-// Main route
 app.get('/', (req, res) => {
   res.send('<h1>Hello World from Vercel! 🚀</h1>');
 });
 
-// Export the app so Vercel can use it as a handler
 module.exports = app;
