fc3c57fcf40536dce10c2e0111bd6115d1ef32ea
KarenGarciaDev <ale1223draa@gmail.com>
2025-11-09 23:10:52 -0500

    Update app.js
    "fix vercel deployment"

--- app.js
+++ app.js
@@ -4,8 +4,13 @@
 const express = require('express');
 const app = express();
 
+// Middleware
+app.use(express.json());
+
+// Main route
 app.get('/', (req, res) => {
   res.send('<h1>Hello World from Vercel! 🚀</h1>');
 });
 
+// Export for serverless
 module.exports = app;
